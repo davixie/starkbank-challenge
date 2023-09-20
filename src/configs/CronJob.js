86ee4195d2e1c54786edeb6b47cfb0cc39c1181f
Davi Xie <davi_xie@hotmail.com>
2023-09-20 02:24:13 -0300

    restoring cron job to set to every 3 hours in 24 hours

--- src/configs/CronJob.js
+++ src/configs/CronJob.js
@@ -1,7 +1,7 @@
 const InvoiceService = require("../services/invoice");
 
 const CronJobConfig = () => {
-  InvoiceService.sendInvoice("*/30 * * * * *", 60000);
+  InvoiceService.sendInvoice();
 };
 
 module.exports = CronJobConfig;
